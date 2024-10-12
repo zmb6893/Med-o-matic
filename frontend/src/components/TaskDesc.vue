73131cd8d6298cde81c16dbd2f0acf777b88de08
jltlam <jlam011235@gmail.com>
2024-10-12 16:18:32 -0400

    changed taskdesc to use taskstore, reduced css

--- frontend/src/components/TaskDesc.vue
+++ frontend/src/components/TaskDesc.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import Task from './Task/Task.vue';
-import { tasks } from '@/data/tasks';
-import { type TaskType } from './Task/Task'
+import { useTaskStore } from '@/stores/TaskStore';
+
+const taskStore = useTaskStore();
+const tasks = taskStore.tasks;
 
 var task = tasks[1];
 
@@ -11,45 +12,22 @@ var task = tasks[1];
 
     <!-- TODO let a practitioner edit the description -->
 
-    <p>Task Name: {{ task.name }}</p>
+    <p>Task Name:</p>
+    <textarea>{{ task.name }}</textarea>
+    
     <p>Task Status {{ task.status }}</p>
     <p>Patient name: {{ task.patient }}</p>
     <p>Location: {{ task.location }}</p>
     <p>Task Owner: {{ task.owner }}</p>
-    <p>Date: {{ task.date }}</p>
+    <p>Date: {{ task.date.toDateString() }}</p>
     <p>Start: {{ task.startTime.hour + ':' + task.startTime.minute + ' ' + task.startTime.type }}</p>
     <p>End: {{ task.endTime.hour + ':' + task.endTime.minute + task.endTime.type }}</p>
 
 </template>
 
 <style scoped>
-.time-stamp {
-	color: gray;
-	width: 5rem;
-	margin: 3rem;
+textarea {
+    width: 100%;
 }
 
-.task-cell {
-	border-left: 1px dotted rgb(190, 190, 190);
-	border-bottom: 1px dotted rgb(203, 203, 203);
-}
-
-.task {
-	width: 90%;
-}
-
-table {
-	background-color: #FBFBFB;
-	border-radius: .2rem;
-}
-
-tr {
-	height: 3rem;
-}
-
-.task-cell {
-	width: 85%;
-	align-items: center;
-	text-align: center;
-}
 </style>
