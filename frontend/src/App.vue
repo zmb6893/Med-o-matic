00a4a6710c2470601fe3b9c289ccd4556bace9f1
Zoe <zmb6893@g.rit.edu>
2024-10-12 10:00:34 -0400

    Initial application created

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Independent Practitioner View</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="container">
+    <RouterView />
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/IndependentPractitionerView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<h1>
+		This is the Independent Practitioner view
+	</h1>
+
+</template>
