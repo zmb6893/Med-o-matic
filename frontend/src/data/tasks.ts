11786ec6f652f25a538d2907c678de70cd674cb2
jltlam <jlam011235@gmail.com>
2024-10-12 15:18:52 -0400

    updated tasks

--- frontend/src/data/tasks.ts
+++ frontend/src/data/tasks.ts
@@ -7,8 +7,168 @@ export const tasks: TaskType[] = [
 	{
 		status: 'UNCLAIMED',
 		name: 'Flip patient',
-		owner: 'Spatula',
-		patient: 'Sue P',
+		owner: '',
+		patient: 'Sue P.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'INCOMPLETE',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 8,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 9,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: '',
+		patient: 'Fung I.',
+		date: new Date(),
+		startTime: {
+			hour: 10,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 11,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Flip patient off',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 1,
+			minute: 0,
+			type: 'PM'
+		},
+		endTime: {
+			hour: 2,
+			minute: 0,
+			type: 'PM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 4,
+			minute: 0,
+			type: 'PM'
+		},
+		endTime: {
+			hour: 5,
+			minute: 0,
+			type: 'PM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
+		date: new Date(),
+		startTime: {
+			hour: 6,
+			minute: 0,
+			type: 'AM'
+		},
+		endTime: {
+			hour: 7,
+			minute: 0,
+			type: 'AM'
+		}
+	}, {
+		status: 'UNCLAIMED',
+		name: 'Follow up on patient prescription',
+		owner: 'Fred',
+		patient: 'Isadora M.',
 		date: new Date(),
 		startTime: {
 			hour: 6,
