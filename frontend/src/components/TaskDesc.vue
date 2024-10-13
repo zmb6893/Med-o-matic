fe62f21806cbb074df0839345b081a14275f3372
jltlam <jlam011235@gmail.com>
2024-10-13 05:07:07 -0400

    can change task fields in desc

--- frontend/src/components/TaskDesc.vue
+++ frontend/src/components/TaskDesc.vue
@@ -1,17 +1,96 @@
 <script lang="ts" setup>
 import { useCurrentUserStore } from '@/stores/CurrentUserStore';
 import { useTaskStore } from '@/stores/TaskStore';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import type { TaskType } from './Task/Task';
 
 const taskStore = useTaskStore();
 const userStore = useCurrentUserStore();
 
-const task = computed(() => taskStore.selectedTask);
+const task = computed({
+    get () { 
+        return taskStore.selectedTask
+    }, 
+    set (updatedTask: TaskType) {
+        taskStore.selectedTask = updatedTask
+    }
+});
+
+const taskName = computed({
+    get () { 
+        return taskStore.selectedTask.name
+    }, 
+    set (name: string) {
+        taskStore.selectedTask.name = name
+    }
+});
+const taskPatient = computed({
+    get () { 
+        return taskStore.selectedTask.patient
+    }, 
+    set (patient: string) {
+        taskStore.selectedTask.patient = patient
+    }
+});
+const taskLocation = computed({
+    get () { 
+        return taskStore.selectedTask.location
+    }, 
+    set (location: string) {
+        taskStore.selectedTask.location = location
+    }
+});
+const taskDate = computed({
+    get () { 
+        return taskStore.selectedTask.date.toDateString()
+    }, 
+    set (date: string) {
+        taskStore.selectedTask.date = new Date(date);
+    }
+});
+const taskStartHr = computed({
+    get () { 
+        return taskStore.selectedTask.startTime.hour
+    }, 
+    set (start: number) {
+        taskStore.selectedTask.startTime.hour = start;
+    }
+});
+const taskStartMin = computed({
+    get () { 
+        return taskStore.selectedTask.startTime.minute.toString().padStart(2, '0')
+    }, 
+    set (start: number) {
+        taskStore.selectedTask.startTime.minute = start;
+    }
+});
+const taskEndHr = computed({
+    get () { 
+        return taskStore.selectedTask.endTime.hour
+    }, 
+    set (end: number) {
+        taskStore.selectedTask.endTime.hour = end;
+    }
+});
+const taskEndMin = computed({
+    get () { 
+        return taskStore.selectedTask.endTime.minute.toString().padStart(2, '0')
+    }, 
+    set (end: number) {
+        taskStore.selectedTask.endTime.minute = end;
+    }
+});
+
+
+const taskModel = ref(task);
 
 const updateSelectedTaskOwner = (isChecked: boolean, username: string) => {
     isChecked ? taskStore.updateTaskOwner(username) : taskStore.updateTaskOwner('')
 }
 
+function get(): import("vue").ComputedGetter<unknown> {
+    throw new Error('Function not implemented.');
+}
 </script>
 
 <template>
@@ -21,7 +100,7 @@ const updateSelectedTaskOwner = (isChecked: boolean, username: string) => {
         Task Description
     </h1>
 
-    <span><h4>Task Name:<textarea>{{ task.name }}</textarea></h4></span>
+    <h4>Task Name:<input v-model="taskName"></h4>
 
     <span>
         <div class="horizontal">
@@ -38,21 +117,15 @@ const updateSelectedTaskOwner = (isChecked: boolean, username: string) => {
         <p><b>Task Status</b> <i>{{ task.status }} </i></p>
     </span>
     
-    <p>Patient name: <input :value="task.patient"> </p>
-    <p>Location: <input :value="task.location"></p>
-    <p>Date: <input :value="task.date.toDateString()"></p>
-    <p>Start: <input :value="task.startTime.hour + ':' + task.startTime.minute.toString().padStart(2, '0')"></p>
-    <p>End: <input :value="task.endTime.hour + ':' + task.endTime.minute.toString().padStart(2, '0')"></p>
-
-    <button onsubmit="save">Save Details</button>
+    <p>Patient name: <input v-model="taskPatient"> </p>
+    <p>Location: <input v-model="taskLocation"></p>
+    <p>Date: <input v-model="taskDate"></p>
+    <p>Start hour: <input v-model="taskStartHr" type="number"> minute: <input v-model="taskStartMin" type="number"></p>
+    <p>End hour: <input v-model="taskEndHr" type="number"> minute: <input v-model="taskEndMin" type="number"></p>
 
 </template>
 
 <style scoped>
-textarea {
-    width: 100%;
-}
-
 .horizontal {
     display: block ruby;
 }
